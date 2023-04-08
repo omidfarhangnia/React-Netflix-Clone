7aa0e4093199efd251dff38f20be44412e06966d
omidfarhangnia <83111084+omidfarhangnia@users.noreply.github.com>
2023-04-08 21:58:28 +0330

    adding projcet style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
 export default function App() {
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+      </Routes>
     </>
   );
 }
---
+++ src/Request.js
@@ -0,0 +1,10 @@
+const key = "eb67a0fa599aa82b6b38ba01d9791551";
+
+const requests = {
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+}
+
+export default requests
