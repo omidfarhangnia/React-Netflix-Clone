8a4a619d3e1bf5f4c53f1d41ae9177648aea7e27
omidfarhangnia <83111084+omidfarhangnia@users.noreply.github.com>
2023-04-09 21:18:21 +0330

    giving the value from db and show it

--- src/Request.js
+++ src/Request.js
@@ -5,6 +5,7 @@ const requests = {
     requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
     requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
     requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+    // https://api.themoviedb.org/3/movie/{movie_id}?api_key=<<api_key>>&language=en-US
 }
 
 export default requests
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,34 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import requests from "../Request";
 
 const Main = () => {
   const [movies, setMovies] = useState([]);
-  return <div></div>;
+  const movie = movies[Math.floor(Math.random() * movies.length)];
+
+  useEffect(() => {
+    axios.get(requests.requestPopular).then((response) => {
+      setMovies(response.data.results);
+    })
+  }, []);
+
+  console.log(movie)
+
+  return (
+    <div className="w-full h-[500px] md:h-[80vh] xl:h-[100vh] text-white">
+      <div className="w-full h-full">
+        <div className="absolute w-full h-full bg-gradient-to-t from-black to-[rgba(0,0,0,0.2)]"></div>
+        <img className="w-full h-full object-cover" src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} alt={movie?.title}/>
+          
+        <div>
+          <h1>{movie?.title}</h1>
+          <button className="border bg-gray-300 text-black border-gray-300 py-2 px-5">Play</button>
+          <button className="border text-white border-gray-300 py-2 px-5 ml-3">Watch Later</button>
+          <p>{movie?.overview}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Main;
