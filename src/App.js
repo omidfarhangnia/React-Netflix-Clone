5c220b9d470a5a18a46a4001b01cb59d045c32ed
omidfarhangnia <83111084+omidfarhangnia@users.noreply.github.com>
2023-04-14 15:20:34 +0330

    adding the route for pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
 import { AuthContextProvider } from "./context/AuthContext";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import Account from "./pages/Account";
 
 export default function App() {
   return (
@@ -10,6 +13,9 @@ export default function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />}/>
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/account" element={<Account />} />
         </Routes>
       </AuthContextProvider>
     </>
