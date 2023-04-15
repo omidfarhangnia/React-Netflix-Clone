5cd4f5631d0cb34ac58883f29dce1622c9400930
omidfarhangnia <83111084+omidfarhangnia@users.noreply.github.com>
2023-04-15 15:39:02 +0330

    completing sign up page

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -17,27 +17,32 @@ const SignUp = () => {
             <form className="flex flex-col py-4">
               <input
                 type="email"
-                className="placeholder:text-white/60 p-4 my-3 bg-white/30 border-none focus-within:outline-none focus-within:bg-white/70 focus-within:text-black focus-within:placeholder:text-black/75"
+                className="placeholder:text-white/60 p-4 my-3 rounded bg-white/30 border-none focus-within:outline-none focus-within:bg-white/70 focus-within:text-black focus-within:placeholder:text-black/75"
                 placeholder="Email"
                 autoComplete="email"
               />
               <input
                 type="password"
-                className="placeholder:text-white/60 p-4 my-3 bg-white/30 border-none focus-within:outline-none focus-within:bg-white/70 focus-within:text-black focus-within:placeholder:text-black/75"
+                className="placeholder:text-white/60 p-4 my-3 rounded bg-white/30 border-none focus-within:outline-none focus-within:bg-white/70 focus-within:text-black focus-within:placeholder:text-black/75"
                 placeholder="Password"
                 autoComplete="current-password"
               />
-              <button className="bg-red-600 py-3 my-6 font-bold">
+              <button className="bg-red-600 py-3 my-6 font-bold rounded">
                 Sign up
               </button>
-              {/* <div>
-            <label>Remember me</label>
-            <input type="checkbox" />
-            </div> */}
-              <div className="flex justify-between text-sm">
-                <span>you have already an account?</span>
-                <Link to="../../login" className="font-bold">
-                  Sign In Now
+              <div className="flex my-3 justify-between select-none">
+                <div>
+                  <input type="checkbox" id="checkBox__Id" />
+                  <label for="checkBox__Id" className="cursor-pointer ml-1">
+                    Remember me
+                  </label>
+                </div>
+                <p className="text-sm text-gray-500">Need Help?</p>
+              </div>
+              <div className="flex justify-between items-center text-sm mt-3">
+                <span className="text-gray-500">already subscribed to netflix</span>
+                <Link to="../../login" className="font-bold bg-red-600 px-4 py-2 rounded">
+                  Sign In
                 </Link>
               </div>
             </form>
